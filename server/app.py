1fc5dd40254de5ed5b4f12f0986c1d9006971fab
Bill Brown <bill.a.brown90@gmail.com>
2023-05-01 18:38:44 -0700

    cosmic challenge

--- server/app.py
+++ server/app.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, request
 from flask_migrate import Migrate
 
-from models import db
+from models import db, Planet, Scientist, Mission
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -17,6 +17,99 @@ db.init_app(app)
 @app.route('/')
 def home():
     return ''
+
+#!SCIENTISTS
+@app.route('/scientists', methods = ['GET', 'POST'])
+def scientists():
+    all_scientists = Scientist.query.all()
+    
+    if request.method == 'GET':
+        scientist_list = [scientist.to_dict() for scientist in all_scientists]
+        response = make_response( scientist_list, 200)              
+        return response
+    
+    elif request.method == 'POST':
+        data = request.get_json()
+        try:
+            new_scientist = Scientist(
+                name=data.get('name'),
+                field_of_study=data.get('field_of_study'), 
+                avatar = data.get('avatar'),
+            )
+            db.session.add(new_scientist)
+            db.session.commit()
+            return new_scientist.to_dict()
+        except ValueError:
+            return {'error': '400: Validation error'}, 400
+    
+    
+#!Scientists by ID
+@app.route('/scientists/<int:id>', methods = ['GET', 'PATCH', 'DELETE'])
+def scientist_by_id(id):
+    scientist= Scientist.query.filter(Scientist.id == id).first()
+    
+    if request.method == 'GET':
+        if scientist:
+            scientist_dict = scientist.to_dict()
+            response = make_response(scientist_dict, 200)         
+            return response
+        else:
+            return {"error": "404: Scientist not found"}, 404
+        
+    elif request.method == "PATCH":
+        if scientist:
+            try:
+                data = request.get_json()
+                
+                for attr in data:
+                    setattr(scientist, attr, data.get(attr))
+                db.session.add(scientist)
+                db.session.commit()
+                
+                scientist_dict = scientist.to_dict()
+                
+                response = make_response(scientist_dict, 200)
+                
+                return response
+            except ValueError:
+                return {'error': '400: Validation Error'}, 400
+        return {"error": "404: Scientist not found"}, 404
+    
+    elif request.method == "DELETE":
+        if scientist:
+            db.session.delete(scientist)
+            db.session.commit()
+            response = make_response("",204)
+            return response
+            
+        return {"error": "404: Scientist not found"}, 404
+    
+#!PLANETS
+@app.route('/planets', methods = ['GET'])
+def planets():
+    all_planets = Planet.query.all()
+    
+    if request.method == 'GET':
+        planet_list = [planet.to_dict(rules=('-missions',)) for planet in all_planets]
+        response = make_response(planet_list, 200)              
+        return response
+
+#!MISSIONS
+@app.route('/missions', methods = ['POST'])
+def missions():      
+    if request.method == 'POST':
+        data = request.get_json()
+        try:
+            new_mission = Mission(
+                name=data.get('name'),
+                scientist_id=data.get('scientist_id'), 
+                planet_id = data.get('planet_id'),
+            )
+            db.session.add(new_mission)
+            db.session.commit()
+            return new_mission.to_dict()
+        except ValueError:
+            return {'error': '400: Validation error'}, 400
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -20,15 +20,108 @@ class Planet(db.Model, SerializerMixin):
     __tablename__ = 'planets'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    distance_from_earth = db.Column(db.String)
+    nearest_star = db.Column(db.String)
+    image = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    
+    #*Relationships
+    missions = db.relationship('Mission', back_populates='planet')  
+    #Throughrelationship 
+    scientists = association_proxy('missions', 'scientist')
+    
+    # #Serialization rules
+    serialize_rules = ('-missions.planet', '-scientists.planets','-created_at','-updated_at',)
+    
+   
+    
+    
+    
+class Mission(db.Model, SerializerMixin):
+    __tablename__ = 'missions'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    scientist_id = db.Column(db.Integer, db.ForeignKey("scientists.id")) 
+    planet_id = db.Column(db.Integer, db.ForeignKey("planets.id")) 
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    
+    #*Relationships
+    planet = db.relationship('Planet', back_populates='missions') 
+    scientist = db.relationship('Scientist', back_populates='missions') 
+    
+    # #Serialization rules
+    serialize_rules = ('-created_at','-updated_at', '-planet.missions', '-scientist.missions')
+    
+    #!Validations
+    @validates('name')
+    def validates_name(self, key, name):
+        if not name:
+            raise ValueError("Must have name")
+        return name
+    
+    @validates('scientist_id')
+    def validates_name(self, key, scientist_id):
+        if not scientist_id:
+            raise ValueError("Must have scientist_id")
+        return scientist_id
+    
+    @validates('planet_id')
+    def validates_name(self, key, planet_id):
+        if not planet_id:
+            raise ValueError("Must have planet_id")
+        return planet_id
+    
+    
+    
 
 class Scientist(db.Model, SerializerMixin):
     __tablename__ = 'scientists'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+    field_of_study=db.Column(db.String, nullable=False)
+    avatar=db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    
+    #*Relationships
+    missions = db.relationship('Mission', back_populates='scientist')  
+    #Throughrelationship 
+    planets = association_proxy('missions', 'planet')
+    
+    #Serialization rules
+    serialize_rules = ('-missions.scientist','-planets.scientists''-created_at','-updated_at',)
+    
+    # !Validations
+    @validates('name')
+    def validates_name(self, key, name):
+        if not name:
+            raise ValueError("Must have name")
+        return name
+    
+    @validates('field_of_study')
+    def validates_name(self, key, field_of_study):
+        if not field_of_study:
+            raise ValueError("Must have a field of study")
+        return field_of_study
+    
+  
+            
+            
+            
+    
+    
+        
+        
+    
+    
+    
+    
 
-class Mission(db.Model, SerializerMixin):
-    __tablename__ = 'missions'
 
-    id = db.Column(db.Integer, primary_key=True)
 
 # add any models you may need. 
